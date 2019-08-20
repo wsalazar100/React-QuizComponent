b6fcffa712b4c32e8a43fb04f6c712be64d08843
William Salazar <soswilo@gmail.com>
2019-08-20 12:30:51 -0500

    Quiz Component

--- src/Quiz.js
+++ src/Quiz.js
@@ -1,4 +1,5 @@
 import React, {Component} from "react";
+import QuizQuestions from "./QuizQuestions.js";
 
 let quizData = require('./quiz_data.json');
 
@@ -10,9 +11,7 @@ class Quiz extends Component {
   render () {
       return (
           <div>
-              <div className="QuizQuestion">
-                  {quizData.quiz_questions[0].instruction_text}
-              </div>
+                <QuizQuestions quiz_questions={quizData.quiz_questions[this.state.quiz_position-1]}/>
           </div>
       )
   }
